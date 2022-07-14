14e2bc9944a59eab2a564bb17867aff8fd4f8bc1
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-14 14:34:34 +0530

    Add files via upload

---
+++ 14-07-22/question 1/Her_Sub.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Her_Sub extends heiracrchial { 
+	public static void main (String agrs[]) {
+		Her_Sub obj=new Her_Sub();
+		obj.aceept();
+         obj.Sub();	
+}
+	public void Sub() {
+		
+      res=a-b;
+	  System.out.println("The substraction of two number is :"+res);	
+	
+
+}
+}
