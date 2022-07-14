998ca46acb2c3ae8ae15d66a33a7c10a31dd2515
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-14 15:13:58 +0530

    Add files via upload

---
+++ Assignment 14-07-22/hiercircal inhertiance/Her_Modulas.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Her_Modulas extends heiracrchial{
+	public static void main (String agrs[]) {
+		Her_Division obj=new Her_Division();
+		obj.aceept();
+         obj.Div();	
+	}
+public void Mod() {
+	res=a%b;
+	System.out.println(" The modulas of two number :"+res);
+}
+}
