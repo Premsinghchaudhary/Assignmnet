d37b95df742071e20844311e33b863e93490f1be
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-14 15:30:54 +0530

    Update use_fib_palidorm.java

--- Assignment 14-07-22/multi_level inhertaince/use_fib_palidorm.java
+++ Assignment 14-07-22/multi_level inhertaince/use_fib_palidorm.java
@@ -2,8 +2,8 @@ package inheritance;
 
 public class use_fib_palidorm extends  multi_palidrom {
      public static void main(String args[]) {
-    	 use_fib_palidorm obj=new use_fib_palidorm ();
-    	 obj.fib();
-    	 obj.Palindrm ();
+    	 use_fib_palidorm obj=new use_fib_palidorm ();//object creat
+    	 obj.fib();//calling the method
+    	 obj.Palindrm ();//calling the method
      }
 }
