11ff684df2a3943bec45151c87fa2c08f10aad1d
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-14 15:29:20 +0530

    Update single_ihn2.java

--- Assignment 14-07-22/single_inher/single_ihn2.java
+++ Assignment 14-07-22/single_inher/single_ihn2.java
@@ -1,9 +1,9 @@
 package inheritance;
 
-public class single_ihn2 extends single_inh {
-	public static void main(String agrs[]) {
-		single_ihn2 obj=new single_ihn2 (); 
-		   obj.Palindrm();	
+public class single_ihn2 extends single_inh {//using the all properties of parents class
+	public static void main(String agrs[]) {//main method
+		single_ihn2 obj=new single_ihn2 (); //creating object 
+		   obj.Palindrm();	//call the method
 	}
 	 
 
