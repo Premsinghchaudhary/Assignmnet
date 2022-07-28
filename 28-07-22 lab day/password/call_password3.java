88ea885b2ce228f078f520640633e0ed3cd25a3f
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-28 18:08:46 +0530

    Add files via upload

---
+++ 28-07-22 lab day/password/call_password3.java
@@ -0,0 +1,8 @@
+package throw_Exception;
+
+public class call_password3 extends Password3 {
+public static void main(String args[]) {
+	call_password3 obj=new call_password3();
+	obj.accept();
+}
+}
