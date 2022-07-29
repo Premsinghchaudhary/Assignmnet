027ac101d15cf096b3d1a1c7ff7daeb5b2473293
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-07-29 11:26:58 +0530

    Update user_Exception1.java

--- 28-07-22 lab day/userId/user_Exception1.java
+++ 28-07-22 lab day/userId/user_Exception1.java
@@ -1,9 +1,9 @@
 package throw_Exception;
 
-public class user_Exception1  extends Exception{//using inhertiance for use the properties of Exception class(Parents class).......
+public class user_def_Exception1  extends Exception{//using inhertiance for use the properties of Exception class(Parents class).......
 	
 	
-	user_Exception1(String msg)//make user defined exception 
+	user_def_Exception1(String msg)//make user defined exception 
 	{
 		super(msg);//used super keyword.......
 	}
