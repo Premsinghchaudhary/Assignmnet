c3a670818516fdb3c5e9c06de4f194035a12329d
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-08-04 14:54:41 +0530

    Update Problem2.java

--- 04-08-2002 lab day/Consumer&producer/Problem2.java
+++ 04-08-2002 lab day/Consumer&producer/Problem2.java
@@ -1,17 +1,19 @@
 package Thursday_lab;
 
 public class Problem2 {//create a class
-	  int materials;
+	//decalred varaibles 
+	int materials;
      boolean available = false;
+	//synchrinized set method
      public synchronized int get()
      {
-           while (available == false)
+           while (available == false)//using while loop.......
            {
-                 try
+                 try//use the try block where exception is show......
                  {
                        wait();
                  }
-                 catch (InterruptedException ie)
+                 catch (InterruptedException ie)//use catch block to handle the exception.........
                  {
                  }
            }
@@ -19,15 +21,16 @@ public class Problem2 {//create a class
            notifyAll();
            return materials;
      }
+	////synchrinized set method
      public synchronized void put(int value)
      {
-           while (available == true)
+           while (available == true)//using while loop.......
            {
-                 try
+                 try//use the try block where exception is show......
                  {
                        wait();
                  }
-                 catch (InterruptedException ie)
+                 catch (InterruptedException ie)//use catch block to handle the exception.........
                  {
                        ie.printStackTrace();
                  }
